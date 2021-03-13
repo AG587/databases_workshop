a7a765f3997f7e77e955401cf59fb1561a3b624f
AG587 <a.glowacka125@gmail.com>
2021-03-13 13:57:04 +0100

    class messages added

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from clcrypto import hash_password
+from datetime import date
 
 
 class User:
@@ -80,3 +81,44 @@ class User:
         self._id = -1
         return True
 
+class Messages:
+    def __init__(self, from_id="", to_id="", text="", creation_data):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self.creation_data = None
+
+    @property
+    def id(self):
+        return self._id
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO messages(from_id, to_id, text, creation_date)
+                            VALUES(%s, %s, %s, %s) RETURNING id"""
+            today = date.today()
+            values = (self.from_id, self.to_id, self.text, today)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()[0]
+            return True
+        else:
+            sql = """UPDATE messages SET from_id=%s, to_id=%s, text=%s, creation_date=%s
+                           WHERE id=%s"""
+            values = (self.from_id, self.to_id, self.text, today, self.id)
+            cursor.execute(sql, values)
+            return True
+
+    @staticmethod
+    def load_all_messages(cursor):
+        sql = "SELECT id, from_id, to_id, text, creation_date FROM messages"
+        messages = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Messages()
+            loaded_message._id = id_
+            loaded_message.from_id = from_id
+            loaded_message.to_id = to_id
+            messages.append(loaded_message)
+        return messages
