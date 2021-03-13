028af2d74892012a8e41c8e4c2267f9c1b897f36
AG587 <a.glowacka125@gmail.com>
2021-03-13 12:16:24 +0100

    next steps

--- create_db.py
+++ create_db.py
@@ -6,24 +6,65 @@ CONFIG = {
     "host": "localhost"
 }
 
-def create_db():
-    connection = None
-    try:
-        connection = connect(**CONFIG)
-        with connection:
-            with connection.cursor as cursor:
-                try:
-                    sql_code = "CREATE DATABASE workshop_db;"
-                    cursor.execute(sql_code)
-                except DatabaseError:
-                    message = "Database already exists."
-    except OperationalError as err:
-        message = f"An error ocurred: {err}."
-    else:
-        connection.close()
-        message = "Database succesfully created."
+sql_create_db = "CREATE DATABASE workshop_db;"
+sql_create_tb_users = "CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(255), hashed_password VARCHAR(80);"
+sql_create_tb_msg = "CREATE TABLE messages (id SERIAL PRIMARY KEY, from_id INT REFERENCES users(id), to_id INT REFERENCES users(id), creation_date TIMESTAMP);"
 
-    return message
+connection = None
+try:
+    connection = connect(**CONFIG)
+    with connection:
+        with connection.cursor() as cursor:
+            try:
+                sql_code = sql_create_db
+                cursor.execute(sql_code)
+            except DatabaseError:
+                message = "Database already exists."
+                print(message)
+except OperationalError as err:
+    message = f"An error ocurred: {err}."
+    print(message)
+else:
+    connection.close()
+    message = "Database succesfully created."
+    print(message)
 
+connection = None
+try:
+    connection = connect(**CONFIG)
+    with connection:
+        with connection.cursor() as cursor:
+            try:
+                sql_code = sql_create_tb_users
+                cursor.execute(sql_code)
+            except DatabaseError:
+                message = "Table already exists."
+                print(message)
+except OperationalError as err:
+    message = f"An error ocurred: {err}."
+    print(message)
+else:
+    connection.close()
+    message = "Table succesfully created."
+    print(message)
+pass
 
+connection = None
+try:
+    connection = connect(**CONFIG)
+    with connection:
+        with connection.cursor() as cursor:
+            try:
+                sql_code = sql_create_tb_msg
+                cursor.execute(sql_code)
+            except DatabaseError:
+                message = "Table already exists."
+                print(message)
+except OperationalError as err:
+    message = f"An error ocurred: {err}."
+    print(message)
+else:
+    connection.close()
+    message = "Table succesfully created."
+    print(message)
 
